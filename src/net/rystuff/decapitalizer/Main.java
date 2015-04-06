c6a20c4d2854d3d5621de804f5c69a36e02300ca
Rydog101 <ryanjeastland@gmail.com>
2015-04-06 12:10:08 +1000

    More Stuff

--- src/net/rystuff/decapitalizer/Main.java
+++ src/net/rystuff/decapitalizer/Main.java
@@ -9,17 +9,15 @@ public class Main
 	    if (args.length==1)
         {
             File[] dirs = new File(args[0]).listFiles();
-
             for (File d : dirs)
             {
-                String[] split = d.toString().split("\\\\");
+                String[] split = d.toString().split(File.separator.toString());
                 String char1 = split[split.length-1].substring(0, 1);
                 String before = split[split.length-1];
                 if (char1.toUpperCase().equals(char1))
                 {
-                    char1 = char1.toLowerCase();
                     StringBuilder newDir = new StringBuilder(split[split.length-1].substring(1));
-                    newDir.insert(0, char1);
+                    newDir.insert(0, char1.toLowerCase());
                     File neW = new File(d.toString().replace(before, newDir));
                     if (d.renameTo(neW))
                         System.out.println("Rename successful! :)");
@@ -31,6 +29,7 @@ public class Main
         else
         {
             System.out.println("Add some arguments man!");
+            System.out.println("1 argument for the directory of files");
         }
     }
 }
